4ae0487a0bd2027647cc5e6c2cfd0ee8438ecf0b
Geordi-Castaneda <jordyorantes9@gmail.com>
2022-01-06 18:25:21 -0600

    Cambios a la versión 12

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import account
-from . import account_move
+from . import account_invoice
 from . import res_company
 from . import fel
 # from . import res_partner
